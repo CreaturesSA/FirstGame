339e8480e6c6cd5cf1121b8c0cc2c075391ff33e
CreaturesSA <creaturegamessa@gmail.com>
2023-12-20 19:36:39 +0000

    merge in main

--- Assets/Scripts/scriptPruebaGithub.cs
+++ Assets/Scripts/scriptPruebaGithub.cs
@@ -8,6 +8,7 @@ public class prueba : MonoBehaviour
     void Start()
     {
         Debug.Log("Este es un script definitivo");
+        Debug.Log("Este es el main");
     }
 
     // Update is called once per frame
